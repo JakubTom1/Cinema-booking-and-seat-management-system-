4db3f2c018cb253a0ed630781d437067f11ce811
JakubTom1 <jakub.tom16@gmail.com>
2025-05-29 17:35:02 +0200

    security

--- backend/crud/users.py
+++ backend/crud/users.py
@@ -1,17 +1,18 @@
-from fastapi import HTTPException, Depends
+from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from backend.schemas import UserCreate
-from backend.crud.security import get_password_hash, get_current_user
+from backend.crud.security import get_password_hash, verify_password
 from backend.models import User
 
+
 # Sprawdzanie roli użytkownika
-def admin_required(current_user: str = Depends(get_current_user)):
+def admin_required(current_user: str = Depends()):
     db_user = get_user_by_login(current_user)
     if db_user.status != 0:
         raise HTTPException(status_code=403, detail="Admin access required")
     return db_user
 
-def staff_required(current_user: str = Depends(get_current_user)):
+def staff_required(current_user: str = Depends()):
     db_user = get_user_by_login(current_user)
     if db_user.status > 1:
         raise HTTPException(status_code=403, detail="Staff access required")
@@ -24,20 +25,12 @@ def get_user_by_login(login: str, db: Session):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-def create_user(db: Session, user: UserCreate):
-    db_user = db.query(User).filter(User.login == user.login).first()
-    if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
-
-    hashed_password = get_password_hash(user.password)
-    db_user = User(
-        first_name=user.first_name,
-        last_name=user.last_name,
-        login=user.login,
-        password=hashed_password,
-        status=user.status
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(User).filter(User.login == username).first()
+    if not user or not verify_password(password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
