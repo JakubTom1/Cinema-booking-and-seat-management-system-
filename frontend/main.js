9d0701fac8f6190ac602e176025c88491efe23d2
Patryk Smajdor <102299402+psmajdor@users.noreply.github.com>
2025-06-06 22:43:42 +0200

    Add userStatus dependency on userDisplay, add film images to homepage

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,20 @@
 function updateUserDisplay() {
   const userDisplay = document.getElementById("userDisplay");
   const login = sessionStorage.getItem("userLogin");
+  const userStatus = parseInt(sessionStorage.getItem("userStatus")); // 0, 1, 2
 
   if (login) {
+    let panelButton = "";
+    if (userStatus === 0) {
+      panelButton = `<button onclick="window.location.href='admin.html'" class="panel-btn">Panel administratora</button>`;
+    } else if (userStatus === 1) {
+      panelButton = `<button onclick="window.location.href='cashier.html'" class="panel-btn">Panel sprzedawcy</button>`;
+    } else if (userStatus === 2) {
+      panelButton = `<button onclick="window.location.href='client.html'" class="panel-btn">Twoje bilety</button>`;
+    }
     userDisplay.innerHTML = `
-      <span>Zalogowany jako: <strong>${login}</strong></span>
+      <span>Zalogowany jako: <strong class="user-name">${login}</strong></span>
+      ${panelButton}
       <button onclick="logout()" class="logout">Wyloguj</button>
     `;
   } else {
